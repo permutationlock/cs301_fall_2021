bd2efa7590b7075c83741d2db62a271a4e69ad13
Linux User <vagrant@alpine.my.domain>
2021-09-22 17:33:19 +0000

    Added array of function pointers example.

---
+++ array_of_function_ptrs/README.md
@@ -0,0 +1,7 @@
+# Function point in struct
+
+Here we use an array of function pointers as a fancy way of computing the
+[Collatz function](https://en.wikipedia.org/wiki/Collatz_conjecture) of a
+positive (unsigned) integer.
+
+ - [main.c on Netrun]()
---
+++ array_of_function_ptrs/main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+typedef unsigned long (*compute_t)(unsigned long);
+
+unsigned long collatz_even(unsigned long n) {
+  return n / 2;
+}
+
+unsigned long collatz_odd(unsigned long n) {
+  return 3 * n + 1;
+}
+
+int main() {
+  compute_t collatz_functions[2];
+  collatz_functions[0] = collatz_even;
+  collatz_functions[1] = collatz_odd;
+
+  unsigned long x = 0;
+  printf("enter a positive integer: \n");
+  scanf("%lu", &x);
+
+  unsigned long n = x;
+  unsigned long count = 0;
+  while(n != 1) {
+    n = collatz_functions[n%2](n);
+    ++count;
+  }
+  printf("it took %lu applications of collatz to reach 1 from %ld\n", count, x);
+  return 0;
+}
